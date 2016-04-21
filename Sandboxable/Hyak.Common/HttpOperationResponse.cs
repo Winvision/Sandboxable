75f30a81a7ea1c3d00a72cbe77d2ef0b3867be9b
Michaël Hompus <Michael.Hompus@Winvision.nl>
2016-04-21 12:58:09 +0200

    - First setup of projects - Adding Travis CI configuration

---
+++ Sandboxable/Hyak.Common/HttpOperationResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace Sandboxable.Hyak.Common
+{
+    /// <summary>
+    /// A standard service response including an HTTP status code and request
+    /// ID.
+    /// </summary>
+    public class HttpOperationResponse
+    {
+        /// <summary>
+        /// Gets or sets the standard HTTP status code from the REST API 
+        /// operations for the Service Management API.
+        /// </summary>
+        public HttpStatusCode StatusCode
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Sandboxable/Hyak.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("Sandboxable Hyak Common Library")]
+[assembly: AssemblyDescription("Provides infrastructure for common error handling, tracing, configuration for REST clients.")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Sandboxable Hyak Common Library")]
+[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
