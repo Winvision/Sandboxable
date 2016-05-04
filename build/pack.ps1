4377a515e863a8c479c6d5250a67a682887556d5
Michaël Hompus <Michael.Hompus@Winvision.nl>
2016-05-04 15:21:51 +0200

    Fix package script to support merges and pull request builds

--- build/pack.ps1
+++ build/pack.ps1
@@ -1,21 +1,29 @@
-# Get git range
+# Get Git range
 $merge = $false
 $parents = (git log -1 --pretty=format:%p)
-if ($parents -like " ") {
-	# Is merge commit
+if ($parents -match " ") {
+	Write-Host "This is a build with a merge"
 	$merge = $true
 	$parents = $parents.replace(" ", "..")
 }
 
 # Version number
 $version = $env:APPVEYOR_BUILD_VERSION
-if ($env:CI -eq "true") {
+if ($env:APPVEYOR_PULL_REQUEST_NUMBER -ne $null) {
+	Write-Host "This is a pull request build"
 	$version = "$version-preview"
 }
+Write-Host "Build version number is $version"
 
 function Pack-Nuget([string]$Name) {
+	Write-Host "Checking if commit contains files for package $name" -NoNewline
 	if ((git log -1 --pretty=format:%H) -eq (git log -1 --pretty=format:%H src/$name) -or ($merge -and (git log $parents --pretty=format:%H src/$name).length -gt 0)) {
+		Write-Host -ForegroundColor Green " -- Yep"
+		Write-Host
 		nuget pack src\$name\$name.csproj -version $version
+		Write-Host
+	} else {
+		Write-Host -ForegroundColor Red " -- Nope"
 	}
 }
 
