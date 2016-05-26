71fce588efd87b038324a0b32b318d9ffcbcfb40
Michaël Hompus <Michael.Hompus@Winvision.nl>
2016-05-26 14:31:11 +0200

    Add TracingAdapter & IServiceOperations

---
+++ src/Hyak.Common/IServiceOperations.cs
@@ -0,0 +1,13 @@
+﻿namespace Sandboxable.Hyak.Common
+{
+  /// <summary>
+  /// Interface used to represent resource groupings of ServiceClient
+  /// operations.
+  /// </summary>
+  /// <typeparam name="TClient">Type of the ServiceClient.</typeparam>
+  public interface IServiceOperations<TClient> where TClient : ServiceClient<TClient>
+  {
+    /// <summary>Gets a reference to the ServiceClient.</summary>
+    TClient Client { get; }
+  }
+}
