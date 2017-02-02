c8736ca1acb01ac7497ebb5a7d9c220f4f79a5a6
Michaël Hompus <Michael.Hompus@Winvision.nl>
2017-02-02 11:29:30 +0100

    Add implementation for IHttpTransportHandlerProvider

---
+++ src/Hyak.Common/Platform/HttpTransportHandlerProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Sandboxable.Hyak.Common.Platform
+{
+    public class HttpTransportHandlerProvider : IHttpTransportHandlerProvider
+    {
+        public HttpTransportHandlerProvider()
+        {
+        }
+
+        public HttpMessageHandler CreateHttpTransportHandler()
+        {
+            return new WebRequestHandler();
+        }
+    }
+}
